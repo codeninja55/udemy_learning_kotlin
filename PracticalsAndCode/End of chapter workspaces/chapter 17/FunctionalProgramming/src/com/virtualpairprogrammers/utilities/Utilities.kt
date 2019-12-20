5d9b62a463e15d7dd82430a81b578643c220c6e2
Andrew Che <andrew@codeninja55.me>
2019-12-21 10:49:32 +1100

    Removing end of chapt files

--- PracticalsAndCode/End of chapter workspaces/chapter 17/FunctionalProgramming/src/com/virtualpairprogrammers/utilities/Utilities.kt
+++
@@ -1,9 +0,0 @@
-package com.virtualpairprogrammers.utilities
-
-fun toSentenceCase(input: String) : String {
-    return input[0].toUpperCase() + input.substring(1)
-}
-
-fun main(args: Array<String>) {
-    println(toSentenceCase("hello"))
-}
--- PracticalsAndCode/End of chapter workspaces/chapter 18/theater/src/main/kotlin/com/virtualpairprogrammers/theater/data/JPAInterfaces.kt
+++
@@ -1,6 +0,0 @@
-package com.virtualpairprogrammers.theater.data
-
-import com.virtualpairprogrammers.theater.domain.Seat
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SeatRepository : JpaRepository<Seat, Long>
